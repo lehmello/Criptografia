fb685826ab9be3b8e108017b45c2051ec5d01145
Leandro Mello <leh.mello.estudante@gmail.com>
2021-01-06 04:32:44 -0300

    first commit

---
+++ criptografador.py
@@ -0,0 +1,561 @@
+key=str(input("Digite a senha para criptografar(APS): "))
+while key!="APS":
+    key=input("Digite a senha correta: ")
+print(key)
+senha=str(input("Digite sua mensagem para criptografar: "))
+psw=[]
+crp=[]
+show=""
+for i in range(len(senha)):
+	psw.append(senha[i])
+print(psw)
+psw.reverse()
+print(psw)
+for i in range(len(psw)):
+	if i%2==0 and i!=0:
+		if (psw[i])=="1":
+			crp.append("ÇiUNCQWE")
+		if (psw[i])=="!":
+			crp.append("UNcq021c")
+		if (psw[i])=="2":
+			crp.append("9UCNqcxq")
+		if (psw[i])=="@":
+			crp.append("ouvyqCXW")
+		if (psw[i])=="3":
+			crp.append("wICunw1c")
+		if (psw[i])=="#":
+			crp.append("0Vbeyçct")
+		if (psw[i])=="4":
+			crp.append("TTTynw12")
+		if (psw[i])=="$":
+			crp.append("2çLIUVWQ")
+		if (psw[i])=="5":
+			crp.append("QÇACOpwh")
+		if (psw[i])=="%":
+			crp.append("UHnew3c0")
+		if (psw[i])=="6":
+			crp.append("fggzQX8G")
+		if (psw[i])=="7":
+			crp.append("bbbcvGX5")
+		if (psw[i])=="&":
+			crp.append("vbQEsqZc")
+		if (psw[i])=="8":
+			crp.append("22014OUc")
+		if (psw[i])=="*":
+			crp.append("25541oMM")
+		if (psw[i])=="9":
+			crp.append("zXHjdq12")
+		if (psw[i])=="(":
+			crp.append("ookmUb1x")
+		if (psw[i])=="0":
+			crp.append("637pNzzQ")
+		if (psw[i])==")":
+			crp.append("MNPQ18fx")
+		if (psw[i])=="-":
+			crp.append("zfnikoaQ")
+		if (psw[i])=="_":
+			crp.append("r55f1dcQ")
+		if (psw[i])=="=":
+			crp.append("bcWQRcoR")
+		if (psw[i])=="+":
+			crp.append("78455VCj")
+		if (psw[i])==":":
+			crp.append("248BBuq4")
+		if (psw[i])=="/":
+			crp.append("v8N25zFD")
+		if (psw[i])=="?":
+			crp.append("vbooiqHY")
+		if (psw[i])=="<":
+			crp.append("tyqsCCA1")
+		if (psw[i])=="[":
+			crp.append("cccqEEX2")
+		if (psw[i])=="{":
+			crp.append("c66erxHA")
+		if (psw[i])=="]":
+			crp.append("hJhHnxq7")
+		if (psw[i])=="}":
+			crp.append("GFTTgzkç")
+		if (psw[i])==";":
+			crp.append("9957oPou")
+		if (psw[i])==".":
+			crp.append("nmYTRQXx")
+		if (psw[i])==",":
+			crp.append("nçÇovgrt")
+		if (psw[i])==">":
+			crp.append("50uYt780")
+		if (psw[i])=="q":
+			crp.append("SDx85jk1")
+		if (psw[i])=="Q":
+			crp.append("1viUN021")
+		if (psw[i])=="w":
+			crp.append("iunc871P")
+		if (psw[i])=="W":
+			crp.append("nnhnIO82")
+		if (psw[i])=="e":
+			crp.append("bnIDHA8X")
+		if (psw[i])=="E":
+			crp.append("vbPOCUNE")
+		if (psw[i])=="r":
+			crp.append("iv8HNWmv")
+		if (psw[i])=="R":
+			crp.append("Mxt4RWXC")
+		if (psw[i])=="t":
+			crp.append("voC2797B")
+		if (psw[i])=="T":
+			crp.append("OQÇc20qe")
+		if (psw[i])=="y":
+			crp.append("DAS2ZOls")
+		if (psw[i])=="Y":
+			crp.append("kkwMEOQ0")
+		if (psw[i])=="u":
+			crp.append("MnOPls1d")
+		if (psw[i])=="U":
+			crp.append("poCYT412")
+		if (psw[i])=="i":
+			crp.append("Vioqx280")
+		if (psw[i])=="I":
+			crp.append("VBioc0q7")
+		if (psw[i])=="o":
+			crp.append("zKunnej1")
+		if (psw[i])=="O":
+			crp.append("5o9YT10q")
+		if (psw[i])=="p":
+			crp.append("pqn8GYAc")
+		if (psw[i])=="P":
+			crp.append("gdfVOuq0")
+		if (psw[i])=="a":
+			crp.append("nhYQUAX0")
+		if (psw[i])=="A":
+			crp.append("OBgyuyqn")
+		if (psw[i])=="s":
+			crp.append("uvQWC0op")
+		if (psw[i])=="S":
+			crp.append("tJunqu77")
+		if (psw[i])=="d":
+			crp.append("rffwwta0")
+		if (psw[i])=="D":
+			crp.append("bkKntqbc")
+		if (psw[i])=="f":
+			crp.append("frnUtrl0")
+		if (psw[i])=="F":
+			crp.append("bllTFGtz")
+		if (psw[i])=="g":
+			crp.append("aC04ffas")
+		if (psw[i])=="G":
+			crp.append("ggtCaq04")
+		if (psw[i])=="h":
+			crp.append("opE04cfa")
+		if (psw[i])=="H":
+			crp.append("c2870qCV")
+		if (psw[i])=="j":
+			crp.append("lçC07qwv")
+		if (psw[i])=="J":
+			crp.append("çmjyQEC0")
+		if (psw[i])=="k":
+			crp.append("hHgHytxb")
+		if (psw[i])=="K":
+			crp.append("poVfqwe6")
+		if (psw[i])=="l":
+			crp.append("nHUQc014")
+		if (psw[i])=="L":
+			crp.append("droo0cq0")
+		if (psw[i])=="ç":
+			crp.append("enda570X")
+		if (psw[i])=="Ç":
+			crp.append("opNQMEc7")
+		if (psw[i])=="z":
+			crp.append("chuiRRA1")
+		if (psw[i])=="Z":
+			crp.append("850BRNjk")
+		if (psw[i])=="x":
+			crp.append("96307BAC")
+		if (psw[i])=="X":
+			crp.append("SVF74OIq")
+		if (psw[i])=="c":
+			crp.append("5070MYUC")
+		if (psw[i])=="C":
+			crp.append("ss74TAmk")
+		if (psw[i])=="v":
+			crp.append("gbmDD7kn")
+		if (psw[i])=="V":
+			crp.append("gcrta09I")
+		if (psw[i])=="b":
+			crp.append("KRpo37uc")
+		if (psw[i])=="B":
+			crp.append("FLOs0wcz")
+		if (psw[i])=="n":
+			crp.append("1027UYNl")
+		if (psw[i])=="N":
+			crp.append("lUYQNC71")
+		if (psw[i])=="m":
+			crp.append("sonMCDG4")
+		if (psw[i])=="M":
+			crp.append("46opqcW7")
+		if (psw[i])==" ":
+			crp.append("263ÇYTew")
+	if i%2!=0:
+		if (psw[i])=="1":
+			crp.append("QWERA7oP")
+		if (psw[i])=="!":
+			crp.append("AqCsdTUC")
+		if (psw[i])=="2":
+			crp.append("8jnQ098N")
+		if (psw[i])=="@":
+			crp.append("pçIQ2zg1")
+		if (psw[i])=="3":
+			crp.append("782OGgu7")
+		if (psw[i])=="#":
+			crp.append("89bgoUQn")
+		if (psw[i])=="4":
+			crp.append("q6c1SOUA")
+		if (psw[i])=="$":
+			crp.append("87OPNfda")
+		if (psw[i])=="5":
+			crp.append("nvT7yQio")
+		if (psw[i])=="%":
+			crp.append("155zo2OQ")
+		if (psw[i])=="6":
+			crp.append("0ÇkjqvcX")
+		if (psw[i])=="7":
+			crp.append("Xsspqubc")
+		if (psw[i])=="&":
+			crp.append("cGTNHNcG")
+		if (psw[i])=="8":
+			crp.append("GpspsTUR")
+		if (psw[i])=="*":
+			crp.append("RkrtHCst")
+		if (psw[i])=="9":
+			crp.append("tUncw123")
+		if (psw[i])=="(":
+			crp.append("3AntmunR")
+		if (psw[i])=="0":
+			crp.append("RANTPrbx")
+		if (psw[i])==")":
+			crp.append("xFwlsQmd")
+		if (psw[i])=="-":
+			crp.append("dFÇamtes")
+		if (psw[i])=="_":
+			crp.append("sdAS85TR")
+		if (psw[i])=="=":
+			crp.append("Rsbarss1")
+		if (psw[i])=="+":
+			crp.append("17KKcbra")
+		if (psw[i])==":":
+			crp.append("aCmma672")
+		if (psw[i])=="/":
+			crp.append("2fddeMTE")
+		if (psw[i])=="?":
+			crp.append("EprtOPLD")
+		if (psw[i])=="<":
+			crp.append("DoldÇCHH")
+		if (psw[i])=="[":
+			crp.append("Hat87Cyn")
+		if (psw[i])=="{":
+			crp.append("n56iuceE")
+		if (psw[i])=="]":
+			crp.append("SZZN3ho5")
+		if (psw[i])=="}":
+			crp.append("FRisk5BN")
+		if (psw[i])==";":
+			crp.append("DEResnw4")
+		if (psw[i])==".":
+			crp.append("ST087me0")
+		if (psw[i])==",":
+			crp.append("çiNNCE36")
+		if (psw[i])==">":
+			crp.append("i0oodDEC")
+		if (psw[i])=="q":
+			crp.append("Tuuudt78")
+		if (psw[i])=="Q":
+			crp.append("Cat3885C")
+		if (psw[i])=="w":
+			crp.append("Gucr46MM")
+		if (psw[i])=="W":
+			crp.append("EVcon167")
+		if (psw[i])=="e":
+			crp.append("dhhe025n")
+		if (psw[i])=="E":
+			crp.append("hUW342nc")
+		if (psw[i])=="r":
+			crp.append("IlkEPRTT")
+		if (psw[i])=="R":
+			crp.append("ILLb8568")
+		if (psw[i])=="t":
+			crp.append("UsRRLIu3")
+		if (psw[i])=="T":
+			crp.append("Ounh4SE1")
+		if (psw[i])=="y":
+			crp.append("IGBN2ney")
+		if (psw[i])=="Y":
+			crp.append("BAepark1")
+		if (psw[i])=="u":
+			crp.append("BRllhnNN")
+		if (psw[i])=="U":
+			crp.append("HPSandDR")
+		if (psw[i])=="i":
+			crp.append("vvo0MMia")
+		if (psw[i])=="I":
+			crp.append("OJjRSlvi")
+		if (psw[i])=="o":
+			crp.append("VVjttaam")
+		if (psw[i])=="O":
+			crp.append("TDV68ze2")
+		if (psw[i])=="p":
+			crp.append("DmVOUdxe")
+		if (psw[i])=="P":
+			crp.append("45Vcntar")
+		if (psw[i])=="a":
+			crp.append("JRonsr8B")
+		if (psw[i])=="A":
+			crp.append("JHNs81MD")
+		if (psw[i])=="s":
+			crp.append("ps08VGTT")
+		if (psw[i])=="S":
+			crp.append("Wammnite")
+		if (psw[i])=="d":
+			crp.append("HTHNGi4v")
+		if (psw[i])=="D":
+			crp.append("WOCmr5hv")
+		if (psw[i])=="f":
+			crp.append("bt1amhrn")
+		if (psw[i])=="F":
+			crp.append("WHTu6ve3")
+		if (psw[i])=="g":
+			crp.append("1dntLVIN")
+		if (psw[i])=="G":
+			crp.append("sxdmm5ND")
+		if (psw[i])=="h":
+			crp.append("69LSTGoG")
+		if (psw[i])=="H":
+			crp.append("hermwu3C")
+		if (psw[i])=="j":
+			crp.append("sKKNDING")
+		if (psw[i])=="J":
+			crp.append("BtIBKRMw")
+		if (psw[i])=="k":
+			crp.append("WNd902dx")
+		if (psw[i])=="K":
+			crp.append("BINmmd35")
+		if (psw[i])=="l":
+			crp.append("RbbcBLKk")
+		if (psw[i])=="L":
+			crp.append("Tqse67MN")
+		if (psw[i])=="ç":
+			crp.append("TNNHammt")
+		if (psw[i])=="Ç":
+			crp.append("EEuma82D")
+		if (psw[i])=="z":
+			crp.append("nSOummqN")
+		if (psw[i])=="Z":
+			crp.append("Mnst52SZ")
+		if (psw[i])=="x":
+			crp.append("iGal43NN")
+		if (psw[i])=="X":
+			crp.append("GuvsMDDA")
+		if (psw[i])=="c":
+			crp.append("LUymtBAO")
+		if (psw[i])=="C":
+			crp.append("ELEmtfyy")
+		if (psw[i])=="v":
+			crp.append("87521fda")
+		if (psw[i])=="V":
+			crp.append("eeeEEnc5")
+		if (psw[i])=="b":
+			crp.append("fl3BTmft")
+		if (psw[i])=="B":
+			crp.append("umPOKm67")
+		if (psw[i])=="n":
+			crp.append("pqTEMmEM")
+		if (psw[i])=="N":
+			crp.append("MButA76G")
+		if (psw[i])=="m":
+			crp.append("sft22223")
+		if (psw[i])=="M":
+			crp.append("fnlMVIE0")
+		if (psw[i])==" ":
+			crp.append("NDian1kr")
+	if i==0:
+		if (psw[i])=="1":
+			crp.append("oCVhrrEg")
+		if (psw[i])=="!":
+			crp.append("RPt00Och")
+		if (psw[i])=="2":
+			crp.append("e85GIIUx")
+		if (psw[i])=="@":
+			crp.append("kllEYmMN")
+		if (psw[i])=="3":
+			crp.append("Chmman74")
+		if (psw[i])=="#":
+			crp.append("lLEun556")
+		if (psw[i])=="4":
+			crp.append("XarFdss2")
+		if (psw[i])=="$":
+			crp.append("BBssu009")
+		if (psw[i])=="5":
+			crp.append("1ViGRO23")
+		if (psw[i])=="%":
+			crp.append("Jsrro930")
+		if (psw[i])=="6":
+			crp.append("esq05TOL")
+		if (psw[i])=="7":
+			crp.append("wwwTDOEL")
+		if (psw[i])=="&":
+			crp.append("BLSy76GE")
+		if (psw[i])=="8":
+			crp.append("01PDbei5")
+		if (psw[i])=="*":
+			crp.append("PodoTTE3")
+		if (psw[i])=="9":
+			crp.append("aLEEWgrn")
+		if (psw[i])=="(":
+			crp.append("rTTAT6uy")
+		if (psw[i])=="0":
+			crp.append("hatyKTE2")
+		if (psw[i])==")":
+			crp.append("ktrBEE96")
+		if (psw[i])=="-":
+			crp.append("grVju452")
+		if (psw[i])=="_":
+			crp.append("nAOlm0Br")
+		if (psw[i])=="=":
+			crp.append("kM0wasUR")
+		if (psw[i])=="+":
+			crp.append("voiC385w")
+		if (psw[i])==":":
+			crp.append("EEuN00sf")
+		if (psw[i])=="/":
+			crp.append("sraQFFcp")
+		if (psw[i])=="?":
+			crp.append("mhnAA435")
+		if (psw[i])=="<":
+			crp.append("ITHNknot")
+		if (psw[i])=="[":
+			crp.append("nFZ8307D")
+		if (psw[i])=="{":
+			crp.append("EDDROa59")
+		if (psw[i])=="]":
+			crp.append("00587IUC")
+		if (psw[i])=="}":
+			crp.append("793LIYUQ")
+		if (psw[i])==";":
+			crp.append("LRTSe851")
+		if (psw[i])==".":
+			crp.append("VRiiArnd")
+		if (psw[i])==",":
+			crp.append("746VYHGQ")
+		if (psw[i])==">":
+			crp.append("JAdsmEI3")
+		if (psw[i])=="q":
+			crp.append("82hgdtqE")
+		if (psw[i])=="Q":
+			crp.append("wppoiYC2")
+		if (psw[i])=="w":
+			crp.append("iUCV65rg")
+		if (psw[i])=="W":
+			crp.append("85963CIU")
+		if (psw[i])=="e":
+			crp.append("765YVTEc")
+		if (psw[i])=="E":
+			crp.append("7893VJUy")
+		if (psw[i])=="r":
+			crp.append("VFDJ1czw")
+		if (psw[i])=="R":
+			crp.append("POIUVMWc")
+		if (psw[i])=="t":
+			crp.append("cjFSQnve")
+		if (psw[i])=="T":
+			crp.append("xxTEVNci")
+		if (psw[i])=="y":
+			crp.append("bnDDA753")
+		if (psw[i])=="Y":
+			crp.append("stndAC01")
+		if (psw[i])=="u":
+			crp.append("EMbUv1w7")
+		if (psw[i])=="U":
+			crp.append("rnsg3AAi")
+		if (psw[i])=="i":
+			crp.append("vuIP765C")
+		if (psw[i])=="I":
+			crp.append("bjuUUdm9")
+		if (psw[i])=="o":
+			crp.append("sh3351KE")
+		if (psw[i])=="O":
+			crp.append("kNAIvv35")
+		if (psw[i])=="p":
+			crp.append("mOakeeUV")
+		if (psw[i])=="P":
+			crp.append("junCE45o")
+		if (psw[i])=="a":
+			crp.append("iuBV88BH")
+		if (psw[i])=="A":
+			crp.append("KOnDio03")
+		if (psw[i])=="s":
+			crp.append("HOHoJTar")
+		if (psw[i])=="S":
+			crp.append("dioO00uy")
+		if (psw[i])=="d":
+			crp.append("MkatoekA")
+		if (psw[i])=="D":
+			crp.append("seAPCrme")
+		if (psw[i])=="f":
+			crp.append("hoadYLER")
+		if (psw[i])=="F":
+			crp.append("NANAb0KE")
+		if (psw[i])=="g":
+			crp.append("ixiKEKA7")
+		if (psw[i])=="G":
+			crp.append("ZER0v379")
+		if (psw[i])=="h":
+			crp.append("zAWDURO3")
+		if (psw[i])=="H":
+			crp.append("vcSBIaqu")
+		if (psw[i])=="j":
+			crp.append("nTENt542")
+		if (psw[i])=="J":
+			crp.append("frEEGRTS")
+		if (psw[i])=="k":
+			crp.append("prbMLeeu")
+		if (psw[i])=="K":
+			crp.append("rsPNSVl3")
+		if (psw[i])=="l":
+			crp.append("nnc20173")
+		if (psw[i])=="L":
+			crp.append("vidAVLWg")
+		if (psw[i])=="ç":
+			crp.append("sorrÇÇOC")
+		if (psw[i])=="Ç":
+			crp.append("psVMOS43")
+		if (psw[i])=="z":
+			crp.append("VTmNEMOX")
+		if (psw[i])=="Z":
+			crp.append("SBMVirxc")
+		if (psw[i])=="x":
+			crp.append("8520Tmmo")
+		if (psw[i])=="X":
+			crp.append("dfentosO")
+		if (psw[i])=="c":
+			crp.append("iBVaus40")
+		if (psw[i])=="C":
+			crp.append("papaVVi5")
+		if (psw[i])=="v":
+			crp.append("estNHAMs")
+		if (psw[i])=="V":
+			crp.append("btGEDOma")
+		if (psw[i])=="b":
+			crp.append("MTbAO5c0")
+		if (psw[i])=="B":
+			crp.append("nvuHSN47")
+		if (psw[i])=="n":
+			crp.append("lkjhCTNw")
+		if (psw[i])=="N":
+			crp.append("jUVEOpv6")
+		if (psw[i])=="m":
+			crp.append("cREivuyt")
+		if (psw[i])=="M":
+			crp.append("cHVas021")
+		if (psw[i])==" ":
+			crp.append("ssEmoSG2")
+for j in (crp):
+        show=show+j
+print(crp)
+print("Sua mensagem criptografada é: {}".format(show))
---
+++ decriptografia.py
@@ -0,0 +1,566 @@
+key=str(input("Digite a senha para decriptografia(APS): "))
+while key!="APS":
+    key=input("Digite a senha correta: ")
+print(key)
+crp=[]
+psw=[]
+show=""
+msg=str(input("Digite sua mensagem criptografada: "))
+n=0
+m=8
+for j in range (len (msg)):
+    psw.append(msg[n:m])
+    n=n+8
+    m=m+8
+for i in range(len(psw)):
+	if i%2==0 and i!=0:
+		if (psw[i])=="ÇiUNCQWE":
+			crp.append("1")
+		if (psw[i])=="UNcq021c":
+			crp.append("!")
+		if (psw[i])=="9UCNqcxq":
+			crp.append("2")
+		if (psw[i])=="ouvyqCXW":
+			crp.append("@")
+		if (psw[i])=="wICunw1c":
+			crp.append("3")
+		if (psw[i])=="0Vbeyçct":
+			crp.append("#")
+		if (psw[i])=="TTTynw12":
+			crp.append("4")
+		if (psw[i])=="2çLIUVWQ":
+			crp.append("$")
+		if (psw[i])=="QÇACOpwh":
+			crp.append("5")
+		if (psw[i])=="UHnew3c0":
+			crp.append("%")
+		if (psw[i])=="fggzQX8G":
+			crp.append("6")
+		if (psw[i])=="bbbcvGX5":
+			crp.append("7")
+		if (psw[i])=="vbQEsqZc":
+			crp.append("&")
+		if (psw[i])=="22014OUc":
+			crp.append("8")
+		if (psw[i])=="25541oMM":
+			crp.append("*")
+		if (psw[i])=="zXHjdq12":
+			crp.append("9")
+		if (psw[i])=="ookmUb1x":
+			crp.append("(")
+		if (psw[i])=="637pNzzQ":
+			crp.append("0")
+		if (psw[i])=="MNPQ18fx":
+			crp.append(")")
+		if (psw[i])=="zfnikoaQ":
+			crp.append("-")
+		if (psw[i])=="r55f1dcQ":
+			crp.append("_")
+		if (psw[i])=="bcWQRcoR":
+			crp.append("=")
+		if (psw[i])=="78455VCj":
+			crp.append("+")
+		if (psw[i])=="248BBuq4":
+			crp.append(":")
+		if (psw[i])=="v8N25zFD":
+			crp.append("/")
+		if (psw[i])=="vbooiqHY":
+			crp.append("?")
+		if (psw[i])=="tyqsCCA1":
+			crp.append("<")
+		if (psw[i])=="cccqEEX2":
+			crp.append("[")
+		if (psw[i])=="c66erxHA":
+			crp.append("{")
+		if (psw[i])=="hJhHnxq7":
+			crp.append("]")
+		if (psw[i])=="GFTTgzkç":
+			crp.append("}")
+		if (psw[i])=="9957oPou":
+			crp.append(";")
+		if (psw[i])=="nmYTRQXx":
+			crp.append(".")
+		if (psw[i])=="nçÇovgrt":
+			crp.append(",")
+		if (psw[i])=="50uYt780":
+			crp.append(">")
+		if (psw[i])=="SDx85jk1":
+			crp.append("q")
+		if (psw[i])=="1viUN021":
+			crp.append("Q")
+		if (psw[i])=="iunc871P":
+			crp.append("w")
+		if (psw[i])=="nnhnIO82":
+			crp.append("W")
+		if (psw[i])=="bnIDHA8X":
+			crp.append("e")
+		if (psw[i])=="vbPOCUNE":
+			crp.append("E")
+		if (psw[i])=="iv8HNWmv":
+			crp.append("r")
+		if (psw[i])=="Mxt4RWXC":
+			crp.append("R")
+		if (psw[i])=="voC2797B":
+			crp.append("t")
+		if (psw[i])=="OQÇc20qe":
+			crp.append("T")
+		if (psw[i])=="DAS2ZOls":
+			crp.append("y")
+		if (psw[i])=="kkwMEOQ0":
+			crp.append("Y")
+		if (psw[i])=="MnOPls1d":
+			crp.append("u")
+		if (psw[i])=="poCYT412":
+			crp.append("U")
+		if (psw[i])=="Vioqx280":
+			crp.append("i")
+		if (psw[i])=="VBioc0q7":
+			crp.append("I")
+		if (psw[i])=="zKunnej1":
+			crp.append("o")
+		if (psw[i])=="5o9YT10q":
+			crp.append("O")
+		if (psw[i])=="pqn8GYAc":
+			crp.append("p")
+		if (psw[i])=="gdfVOuq0":
+			crp.append("P")
+		if (psw[i])=="nhYQUAX0":
+			crp.append("a")
+		if (psw[i])=="OBgyuyqn":
+			crp.append("A")
+		if (psw[i])=="uvQWC0op":
+			crp.append("s")
+		if (psw[i])=="tJunqu77":
+			crp.append("S")
+		if (psw[i])=="rffwwta0":
+			crp.append("d")
+		if (psw[i])=="bkKntqbc":
+			crp.append("D")
+		if (psw[i])=="frnUtrl0":
+			crp.append("f")
+		if (psw[i])=="bllTFGtz":
+			crp.append("F")
+		if (psw[i])=="aC04ffas":
+			crp.append("g")
+		if (psw[i])=="ggtCaq04":
+			crp.append("G")
+		if (psw[i])=="opE04cfa":
+			crp.append("h")
+		if (psw[i])=="c2870qCV":
+			crp.append("H")
+		if (psw[i])=="lçC07qwv":
+			crp.append("j")
+		if (psw[i])=="çmjyQEC0":
+			crp.append("J")
+		if (psw[i])=="hHgHytxb":
+			crp.append("k")
+		if (psw[i])=="poVfqwe6":
+			crp.append("K")
+		if (psw[i])=="nHUQc014":
+			crp.append("l")
+		if (psw[i])=="droo0cq0":
+			crp.append("L")
+		if (psw[i])=="enda570X":
+			crp.append("ç")
+		if (psw[i])=="opNQMEc7":
+			crp.append("Ç")
+		if (psw[i])=="chuiRRA1":
+			crp.append("z")
+		if (psw[i])=="850BRNjk":
+			crp.append("Z")
+		if (psw[i])=="96307BAC":
+			crp.append("x")
+		if (psw[i])=="SVF74OIq":
+			crp.append("X")
+		if (psw[i])=="5070MYUC":
+			crp.append("c")
+		if (psw[i])=="ss74TAmk":
+			crp.append("C")
+		if (psw[i])=="gbmDD7kn":
+			crp.append("v")
+		if (psw[i])=="gcrta09I":
+			crp.append("V")
+		if (psw[i])=="KRpo37uc":
+			crp.append("b")
+		if (psw[i])=="FLOs0wcz":
+			crp.append("B")
+		if (psw[i])=="1027UYNl":
+			crp.append("n")
+		if (psw[i])=="lUYQNC71":
+			crp.append("N")
+		if (psw[i])=="sonMCDG4":
+			crp.append("m")
+		if (psw[i])=="46opqcW7":
+			crp.append("M")
+		if (psw[i])=="263ÇYTew":
+			crp.append(" ")  
+	if i%2!=0:
+		if (psw[i])=="QWERA7oP":
+			crp.append("1")
+		if (psw[i])=="AqCsdTUC":
+			crp.append("!")
+		if (psw[i])=="8jnQ098N":
+			crp.append("2")
+		if (psw[i])=="pçIQ2zg1":
+			crp.append("@")
+		if (psw[i])=="782OGgu7":
+			crp.append("3")
+		if (psw[i])=="89bgoUQn":
+			crp.append("#")
+		if (psw[i])=="q6c1SOUA":
+			crp.append("4")
+		if (psw[i])=="87OPNfda":
+			crp.append("$")
+		if (psw[i])=="nvT7yQio":
+			crp.append("5")
+		if (psw[i])=="155zo2OQ":
+			crp.append("%")
+		if (psw[i])=="0ÇkjqvcX":
+			crp.append("6")
+		if (psw[i])=="Xsspqubc":
+			crp.append("7")
+		if (psw[i])=="cGTNHNcG":
+			crp.append("&")
+		if (psw[i])=="GpspsTUR":
+			crp.append("8")
+		if (psw[i])=="RkrtHCst":
+			crp.append("*")
+		if (psw[i])=="tUncw123":
+			crp.append("9")
+		if (psw[i])=="3AntmunR":
+			crp.append("(")
+		if (psw[i])=="RANTPrbx":
+			crp.append("0")
+		if (psw[i])=="xFwlsQmd":
+			crp.append(")")
+		if (psw[i])=="dFÇamtes":
+			crp.append("-")
+		if (psw[i])=="sdAS85TR":
+			crp.append("_")
+		if (psw[i])=="Rsbarss1":
+			crp.append("=")
+		if (psw[i])=="17KKcbra":
+			crp.append("+")
+		if (psw[i])=="aCmma672":
+			crp.append(":")
+		if (psw[i])=="2fddeMTE":
+			crp.append("/")
+		if (psw[i])=="EprtOPLD":
+			crp.append("?")
+		if (psw[i])=="DoldÇCHH":
+			crp.append("<")
+		if (psw[i])=="Hat87Cyn":
+			crp.append("[")
+		if (psw[i])=="n56iuceE":
+			crp.append("{")
+		if (psw[i])=="SZZN3ho5":
+			crp.append("]")
+		if (psw[i])=="FRisk5BN":
+			crp.append("}")
+		if (psw[i])=="DEResnw4":
+			crp.append(";")
+		if (psw[i])=="ST087me0":
+			crp.append(".")
+		if (psw[i])=="çiNNCE36":
+			crp.append(",")
+		if (psw[i])=="i0oodDEC":
+			crp.append(">")
+		if (psw[i])=="Tuuudt78":
+			crp.append("q")
+		if (psw[i])=="Cat3885C":
+			crp.append("Q")
+		if (psw[i])=="Gucr46MM":
+			crp.append("w")
+		if (psw[i])=="EVcon167":
+			crp.append("W")
+		if (psw[i])=="dhhe025n":
+			crp.append("e")
+		if (psw[i])=="hUW342nc":
+			crp.append("E")
+		if (psw[i])=="IlkEPRTT":
+			crp.append("r")
+		if (psw[i])=="ILLb8568":
+			crp.append("R")
+		if (psw[i])=="UsRRLIu3":
+			crp.append("t")
+		if (psw[i])=="Ounh4SE1":
+			crp.append("T")
+		if (psw[i])=="IGBN2ney":
+			crp.append("y")
+		if (psw[i])=="BAepark1":
+			crp.append("Y")
+		if (psw[i])=="BRllhnNN":
+			crp.append("u")
+		if (psw[i])=="HPSandDR":
+			crp.append("U")
+		if (psw[i])=="vvo0MMia":
+			crp.append("i")
+		if (psw[i])=="OJjRSlvi":
+			crp.append("I")
+		if (psw[i])=="VVjttaam":
+			crp.append("o")
+		if (psw[i])=="TDV68ze2":
+			crp.append("O")
+		if (psw[i])=="DmVOUdxe":
+			crp.append("p")
+		if (psw[i])=="45Vcntar":
+			crp.append("P")
+		if (psw[i])=="JRonsr8B":
+			crp.append("a")
+		if (psw[i])=="JHNs81MD":
+			crp.append("A")
+		if (psw[i])=="ps08VGTT":
+			crp.append("s")
+		if (psw[i])=="Wammnite":
+			crp.append("S")
+		if (psw[i])=="HTHNGi4v":
+			crp.append("d")
+		if (psw[i])=="WOCmr5hv":
+			crp.append("D")
+		if (psw[i])=="bt1amhrn":
+			crp.append("f")
+		if (psw[i])=="WHTu6ve3":
+			crp.append("F")
+		if (psw[i])=="1dntLVIN":
+			crp.append("g")
+		if (psw[i])=="sxdmm5ND":
+			crp.append("G")
+		if (psw[i])=="69LSTGoG":
+			crp.append("h")
+		if (psw[i])=="hermwu3C":
+			crp.append("H")
+		if (psw[i])=="sKKNDING":
+			crp.append("j")
+		if (psw[i])=="BtIBKRMw":
+			crp.append("J")
+		if (psw[i])=="WNd902dx":
+			crp.append("k")
+		if (psw[i])=="BINmmd35":
+			crp.append("K")
+		if (psw[i])=="RbbcBLKk":
+			crp.append("l")
+		if (psw[i])=="Tqse67MN":
+			crp.append("L")
+		if (psw[i])=="TNNHammt":
+			crp.append("ç")
+		if (psw[i])=="EEuma82D":
+			crp.append("Ç")
+		if (psw[i])=="nSOummqN":
+			crp.append("z")
+		if (psw[i])=="Mnst52SZ":
+			crp.append("Z")
+		if (psw[i])=="iGal43NN":
+			crp.append("x")
+		if (psw[i])=="GuvsMDDA":
+			crp.append("X")
+		if (psw[i])=="LUymtBAO":
+			crp.append("c")
+		if (psw[i])=="ELEmtfyy":
+			crp.append("C")
+		if (psw[i])=="87521fda":
+			crp.append("v")
+		if (psw[i])=="eeeEEnc5":
+			crp.append("V")
+		if (psw[i])=="fl3BTmft":
+			crp.append("b")
+		if (psw[i])=="umPOKm67":
+			crp.append("B")
+		if (psw[i])=="pqTEMmEM":
+			crp.append("n")
+		if (psw[i])=="MButA76G":
+			crp.append("N")
+		if (psw[i])=="sft22223":
+			crp.append("m")
+		if (psw[i])=="fnlMVIE0":
+			crp.append("M")
+		if (psw[i])=="NDian1kr":
+			crp.append(" ")    
+	if i==0:
+		if (psw[i])=="oCVhrrEg":
+			crp.append("1")
+		if (psw[i])=="RPt00Och":
+			crp.append("!")
+		if (psw[i])=="e85GIIUx":
+			crp.append("2")
+		if (psw[i])=="kllEYmMN":
+			crp.append("@")
+		if (psw[i])=="Chmman74":
+			crp.append("3")
+		if (psw[i])=="lLEun556":
+			crp.append("#")
+		if (psw[i])=="XarFdss2":
+			crp.append("4")
+		if (psw[i])=="BBssu009":
+			crp.append("$")
+		if (psw[i])=="1ViGRO23":
+			crp.append("5")
+		if (psw[i])=="Jsrro930":
+			crp.append("%")
+		if (psw[i])=="esq05TOL":
+			crp.append("6")
+		if (psw[i])=="wwwTDOEL":
+			crp.append("7")
+		if (psw[i])=="BLSy76GE":
+			crp.append("&")
+		if (psw[i])=="01PDbei5":
+			crp.append("8")
+		if (psw[i])=="PodoTTE3":
+			crp.append("*")
+		if (psw[i])=="aLEEWgrn":
+			crp.append("9")
+		if (psw[i])=="rTTAT6uy":
+			crp.append("(")
+		if (psw[i])=="hatyKTE2":
+			crp.append("0")
+		if (psw[i])=="ktrBEE96":
+			crp.append(")")
+		if (psw[i])=="grVju452":
+			crp.append("-")
+		if (psw[i])=="nAOlm0Br":
+			crp.append("_")
+		if (psw[i])=="kM0wasUR":
+			crp.append("=")
+		if (psw[i])=="voiC385w":
+			crp.append("+")
+		if (psw[i])=="EEuN00sf":
+			crp.append(":")
+		if (psw[i])=="sraQFFcp":
+			crp.append("/")
+		if (psw[i])=="mhnAA435":
+			crp.append("?")
+		if (psw[i])=="ITHNknot":
+			crp.append("<")
+		if (psw[i])=="nFZ8307D":
+			crp.append("[")
+		if (psw[i])=="EDDROa59":
+			crp.append("{")
+		if (psw[i])=="00587IUC":
+			crp.append("]")
+		if (psw[i])=="793LIYUQ":
+			crp.append("}")
+		if (psw[i])=="LRTSe851":
+			crp.append(";")
+		if (psw[i])=="VRiiArnd":
+			crp.append(".")
+		if (psw[i])=="746VYHGQ":
+			crp.append(",")
+		if (psw[i])=="JAdsmEI3":
+			crp.append(">")
+		if (psw[i])=="82hgdtqE":
+			crp.append("q")
+		if (psw[i])=="wppoiYC2":
+			crp.append("Q")
+		if (psw[i])=="iUCV65rg":
+			crp.append("w")
+		if (psw[i])=="85963CIU":
+			crp.append("W")
+		if (psw[i])=="765YVTEc":
+			crp.append("e")
+		if (psw[i])=="7893VJUy":
+			crp.append("E")
+		if (psw[i])=="VFDJ1czw":
+			crp.append("r")
+		if (psw[i])=="POIUVMWc":
+			crp.append("R")
+		if (psw[i])=="cjFSQnve":
+			crp.append("t")
+		if (psw[i])=="xxTEVNci":
+			crp.append("T")
+		if (psw[i])=="bnDDA753":
+			crp.append("y")
+		if (psw[i])=="stndAC01":
+			crp.append("Y")
+		if (psw[i])=="EMbUv1w7":
+			crp.append("u")
+		if (psw[i])=="rnsg3AAi":
+			crp.append("U")
+		if (psw[i])=="vuIP765C":
+			crp.append("i")
+		if (psw[i])=="bjuUUdm9":
+			crp.append("I")
+		if (psw[i])=="sh3351KE":
+			crp.append("o")
+		if (psw[i])=="kNAIvv35":
+			crp.append("O")
+		if (psw[i])=="mOakeeUV":
+			crp.append("p")
+		if (psw[i])=="junCE45o":
+			crp.append("P")
+		if (psw[i])=="iuBV88BH":
+			crp.append("a")
+		if (psw[i])=="KOnDio03":
+			crp.append("A")
+		if (psw[i])=="HOHoJTar":
+			crp.append("s")
+		if (psw[i])=="dioO00uy":
+			crp.append("S")
+		if (psw[i])=="MkatoekA":
+			crp.append("d")
+		if (psw[i])=="seAPCrme":
+			crp.append("D")
+		if (psw[i])=="hoadYLER":
+			crp.append("f")
+		if (psw[i])=="NANAb0KE":
+			crp.append("F")
+		if (psw[i])=="ixiKEKA7":
+			crp.append("g")
+		if (psw[i])=="ZER0v379":
+			crp.append("G")
+		if (psw[i])=="zAWDURO3":
+			crp.append("h")
+		if (psw[i])=="vcSBIaqu":
+			crp.append("H")
+		if (psw[i])=="nTENt542":
+			crp.append("j")
+		if (psw[i])=="frEEGRTS":
+			crp.append("J")
+		if (psw[i])=="prbMLeeu":
+			crp.append("k")
+		if (psw[i])=="rsPNSVl3":
+			crp.append("K")
+		if (psw[i])=="nnc20173":
+			crp.append("l")
+		if (psw[i])=="vidAVLWg":
+			crp.append("L")
+		if (psw[i])=="sorrÇÇOC":
+			crp.append("ç")
+		if (psw[i])=="psVMOS43":
+			crp.append("Ç")
+		if (psw[i])=="VTmNEMOX":
+			crp.append("z")
+		if (psw[i])=="SBMVirxc":
+			crp.append("Z")
+		if (psw[i])=="8520Tmmo":
+			crp.append("x")
+		if (psw[i])=="dfentosO":
+			crp.append("X")
+		if (psw[i])=="iBVaus40":
+			crp.append("c")
+		if (psw[i])=="papaVVi5":
+			crp.append("C")
+		if (psw[i])=="estNHAMs":
+			crp.append("v")
+		if (psw[i])=="btGEDOma":
+			crp.append("V")
+		if (psw[i])=="MTbAO5c0":
+			crp.append("b")
+		if (psw[i])=="nvuHSN47":
+			crp.append("B")
+		if (psw[i])=="lkjhCTNw":
+			crp.append("n")
+		if (psw[i])=="jUVEOpv6":
+			crp.append("N")
+		if (psw[i])=="cREivuyt":
+			crp.append("m")
+		if (psw[i])=="cHVas021":
+			crp.append("M")
+		if (psw[i])=="ssEmoSG2":
+			crp.append(" ")    
+print(crp)
+crp.reverse()
+print(crp)
+for j in (crp):
+        show=show+j
+print("Sua mensagem decriptografada é: {}".format(show))
+
+
